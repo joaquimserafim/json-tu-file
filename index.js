e7e2a56a3921daa91aba8f5664dddff8461dc330
joaquimserafim <joaquim.serafim@gmail.com>
2014-11-22 00:44:35 +0000

    v1.3

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-module.exports = require('./lib');
+'use strict';
+
+var fs = require('fs');
+var stringify = require('json-stringify-safe');
+
+//
+//  json-tu-file
+//
+var jsonTuFile = module.exports;
+
+jsonTuFile._spaces = 2;
+
+jsonTuFile.jsonParse = function _parse(str) {
+  try {
+    return JSON.parse(str);
+  } catch (e) {
+    return;
+  }
+};
+
+jsonTuFile.stringify = stringify;
+
+jsonTuFile.readFile = function readFile(file, cb) {
+  var self = this;
+  fs.readFile(file, 'utf8', function readFileCb(err, data) {
+    if (err) {
+      cb(err);
+    } else {
+      var obj = self.jsonParse(data);
+
+      if (!obj) {
+        cb(new Error('Invalid JSON structure!'));
+      } else {
+        cb(null, obj);
+      }
+    }
+  });
+};
+
+jsonTuFile.readFileSync = function readFileSync(file) {
+  return this.jsonParse(fs.readFileSync(file, 'utf8'));
+};
+
+jsonTuFile.writeFile = function(obj, file, options, cb) {
+  if (!cb) {
+    cb = options;
+    options = null;
+  }
+
+  var str = this.stringify(obj, null, this._spaces);
+  fs.writeFile(file, str, options, cb);
+};
+
+jsonTuFile.writeFileSync = function writeFileSync(obj, file, options) {
+  var str = this.stringify(obj, null, this._spaces);
+  fs.writeFileSync(file, str, options);
+};
--- test/index.js
+++ test/index.js
@@ -1,50 +1,86 @@
+'use strict';
+
 var test = require('tape');
-var JSONFile = require('../');
+var json2file = require('../index');
+var join = require('path').join;
 
+var obj = {
+  n:  8,
+  msg: 'Hello World 7bc59b8e-8a3f-2d04-62eb-8c9f5c39606f',
+  tmx: 1388775322979
+};
 
-test('write', function (t) {
-  t.plan(1);
+var file = join(__dirname, 'fixtures/write.json');
 
-  var obj = {"n":8,"msg":"Hello World 7bc59b8e-8a3f-2d04-62eb-8c9f5c39606f","tmx":1388775322979};
-
-  JSONFile.writeFile(obj, 'test/write.json', function (err) {
-    if (err) t.error(err, 'write json to file');
-
-    t.pass('write json object to file.');
+test('write a json into a file', function(assert) {
+  json2file.writeFile(obj, file, function(err) {
+    if (err) {
+      assert.fail(err);
+    } else {
+      assert.ok('wrote a json object with success into a file.');
+      assert.end();
+    }
   });
 });
 
+test('write a json into a file with options', function(assert) {
+  json2file.writeFile(obj, file, {encoding: 'utf8'}, function(err) {
+    if (err) {
+      assert.fail(err);
+    } else {
+      assert.ok('wrote a json object with success into a file.');
+      assert.end();
+    }
+  });
+});
 
-test('write sync', function (t) {
-  t.plan(1);
-
-  var obj = {"n":8,"msg":"Hello World 7bc59b8e-8a3f-2d04-62eb-8c9f5c39606f","tmx":1388775322979};
-
+test('write sync a json into a file', function(assert) {
   try {
-    JSONFile.writeFileSync(obj, 'test/write_sync.json');
-    t.pass('write sync json object to file.');
-  } catch (err) {
-    t.error(err);
+    json2file.writeFileSync(obj, file);
+    assert.ok('wrote a json object with success into a file.');
+    assert.end();
+  } catch (ex) {
+    assert.fail(ex);
   }
 });
 
-test('read', function (t) {
-  t.plan(1);
-
-  JSONFile.readFile('test/write.json', function (err, data) {
-    if (err) t.error(err, 'write json to file');
-
-    t.deepEqual(typeof data, 'object', JSON.stringify(data));
+test('read a json from a file', function(assert) {
+  json2file.readFile(file, function(err, json) {
+    if (err) {
+      assert.fail(err);
+    } else {
+      assert.deepEqual(typeof json, 'object');
+      assert.end();
+    }
   });
 });
 
-test('read sync', function (t) {
-  t.plan(1);
+test('try to read a json from a file don\'t exist', function(assert) {
+  var badFile = join(__dirname, 'fixtures/writee.json');
+  json2file.readFile(badFile, function(err) {
+    if (err) {
+      assert.pass(err);
+      assert.end();
+    }
+  });
+});
 
+test('try to read a bad json from a file' , function(assert) {
+  var badJson = join(__dirname, 'fixtures/bad_json_file.json');
+  json2file.readFile(badJson, function(err) {
+    if (err) {
+      assert.pass(err);
+      assert.end();
+    }
+  });
+});
+
+test('read sync a json from a file', function(assert) {
   try {
-    var data = JSONFile.readFileSync('test/write_sync.json');
-    t.deepEqual(typeof data, 'object', JSON.stringify(data));
-  } catch (err) {
-    t.error(err);
+    var json = json2file.readFileSync(file);
+    assert.deepEqual(typeof json, 'object');
+    assert.end();
+  } catch (ex) {
+    assert.fail(ex);
   }
 });
